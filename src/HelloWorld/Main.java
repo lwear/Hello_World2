f848934276ef6d5a28523dc5779a4f1450cc97ba
1414367 <1414367@W49L203S20.sd61.bc.ca>
2018-09-20 11:04:57 -0700

    Test?

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("foo");
+        System.out.println("hello this is Ryan");
         int length = 0;
-        System.out.println("foo");
+        System.out.println("hello this is Ryan");
         String [] fileContents = getFileContents("nums.txt");
         for (int i = 0; i < fileContents.length; i++) {
             System.out.println(fileContents[i]);
